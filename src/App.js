39d0ac3315433daeb7a7ee59b794f82d4aed0f31
Samirul Alam <samirulalam372@gmail.com>
2021-05-10 03:01:53 +0600

    completed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import WebcamCapture from './WebcamCapture';
 import {
@@ -8,24 +8,58 @@ import {
 } from "react-router-dom";
 import Preview from './Preview';
 import Chats from './Chats';
+import ChatView from './ChatView';
+import { login, logout, selectUser } from './features/appSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import Login from './Login';
+import { auth } from './firebase';
 
 function App() {
+  const dispatch=useDispatch()
+  const user=useSelector(selectUser);
+  useEffect(()=>{
+      auth.onAuthStateChanged((authUser)=>{
+        if(authUser){
+          dispatch(login({
+              username: authUser.displayName,
+                profilePic:authUser.photoURL,
+                id: authUser.uid
+          }))
+        }else{
+          dispatch(logout())
+        }
+      })
+  },[])
   return (
     <div className="app">
       <Router>
-      <div className="app__body">      
-        <Switch>
-        <Route path="/chats">
-          <Chats />
-          </Route>
-        <Route path="/preview">
-          <Preview />
-          </Route>
-          <Route exact path="/">
-          <WebcamCapture />
-          </Route>
-        </Switch>
-      </div>
+        {!user ? (
+          <Login />
+        ):(
+          <>
+          <img className="app__logo" src="https://pbs.twimg.com/profile_images/1324384358418042880/A-ENfuMC_400x400.jpg" />
+          <div className="app__body">  
+          <div className="body__background">
+          <Switch>
+          <Route path="/chats/view">
+            <ChatView />
+            </Route>
+          <Route path="/chats">
+            <Chats />
+            </Route>
+          <Route path="/preview">
+            <Preview />
+            </Route>
+            <Route exact path="/">
+            <WebcamCapture />
+            </Route>
+          </Switch>
+            </div>    
+          
+        </div>
+        </>
+        )}
+      
     </Router>
       
     </div>
--- src/Chats.js
+++ src/Chats.js
@@ -1,10 +1,57 @@
-import React from 'react';
-import "./Chats.css"
+import { Avatar, Radio } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+import "./Chats.css";
+import SearchIcon from '@material-ui/icons/Search';
+import ChatBubbleIcon from '@material-ui/icons/ChatBubble';
+import Chat from './Chat';
+import { auth, db } from './firebase';
+import { selectUser } from './features/appSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import RadioButtonUncheckedIcon from '@material-ui/icons/RadioButtonUnchecked';
+import { useHistory } from 'react-router';
+import { resetCameraImage } from './features/cameraSlice';
 
 function Chats() {
+    const [posts,setPosts]=useState([]);
+    const user=useSelector(selectUser);
+    const history=useHistory();
+    const dispatch = useDispatch()
+    useEffect(()=>{
+        db.collection("posts").orderBy("timestamp","desc").onSnapshot((snapshot)=>setPosts(snapshot.docs.map((doc)=>({
+            id: doc.id,
+            data:doc.data(),
+        }))
+        )
+        )
+    },[])
+
+    const takeSnap=()=>{
+        history.push("/")
+    }
     return (
         <div className="chats">
-            
+            <div className="chats__header">
+                <Avatar src={user.profilePic} onClick={()=>auth.signOut()} className="chats__avatar" />
+                <div className="chats__search">
+                    <SearchIcon className="chats__searchIcon" />
+                    <input type="text" placeholder="Friends" />
+                </div>
+                <ChatBubbleIcon className="chats__chatIcon" />
+            </div>
+            <div className="chats__posts">
+                {posts.map(({id,data:{profilePic,username,timestamp,read,imageUrl}}) =>(
+                    <Chat 
+                        key={id}
+                        id={id}
+                        username={username}
+                        timestamp={timestamp}
+                        imageUrl={imageUrl}
+                        read={read}
+                        profilePic={profilePic}
+                    />
+                ))}
+            </div>
+                <RadioButtonUncheckedIcon className="chats__takePicIcon" onClick={takeSnap} fontSize="large" />
         </div>
     )
 }
